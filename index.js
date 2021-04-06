5c551d9d3ef08786c1191d0505df596fb99599fd
Eric Nganga <ericmainaedits@gmail.com>
2021-04-06 18:39:05 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(namesArray, event) {
+   const thankYouMessage = [] 
+   //namesArray[`Thank you ${name} for ${event}']
+   for(let i=0;i < namesArray.length; i++ ) {
+thankYouMessage.push (`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+   }
+  return thankYouMessage;
+}
+
+function countDown(startingNumber) {
+    while (startingNumber > 0) {
+        console.log(startingNumber);
+        startingNumber -= 1;
+    }
+    console.log(startingNumber);
+}
